e8c1803444c69d93335716317bf3da82d02080f5
dumon <dumon@Dell>
2018-04-16 20:37:25 -0300

    Inserção de código

--- UltraEmojiCombateJava/src/ultraemojicombatejava/UltraEmojiCombateJava.java
+++ UltraEmojiCombateJava/src/ultraemojicombatejava/UltraEmojiCombateJava.java
@@ -15,7 +15,19 @@ public class UltraEmojiCombateJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Lutador l[] = new Lutador[6];
+        l[0] = new Lutador("Alex", "Brasilia", 34, (float) 1.70, (float) 60.9, 1, 1, 1);
+        l[1] = new Lutador("Teste", "Brasilia", 18, (float) 1.70, (float) 68.9, 1, 1, 1);
+        l[2] = new Lutador("eleno", "Brasilia", 40, (float) 1.70, (float) 70.9, 1, 1, 1);
+        l[3] = new Lutador("Digo", "Brasilia", 35, (float) 1.70, (float) 80.9, 1, 1, 1);
+        l[4] = new Lutador("joe", "Brasilia", 55, (float) 1.70, (float) 98.9, 1, 1, 1);
+        l[5] = new Lutador("Roi", "Brasilia", 26, (float) 1.70, (float) 58.9, 1, 1, 1);
+
+        for (int i = 0; i < l.length; i++) {
+            l[i].apresentar();
+
+        }
+
     }
-    
+
 }
